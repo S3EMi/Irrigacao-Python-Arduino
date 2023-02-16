508c5efeb9752842761f29da7907f60c1b74115d
Matheus Barros <89200586+S3EMi@users.noreply.github.com>
2023-02-16 16:47:44 -0300

    Update ardcont.py

--- ardcont.py
+++ ardcont.py
@@ -7,10 +7,6 @@ import time
 from pyfirmata import Arduino, util, SERVO, STRING_DATA
 import os
 
-# Função de pausa bruta utilizando um prompt de INPUT
-def pause():
-    input("\nPressione qualquer tecla para continuar...")
-
 # Função para limpar a janela
 def clearConsole():
     os.system('cls')
@@ -31,7 +27,7 @@ title = r.html.find('span.BBwThe', first=True).text
 # No site falava que era bom começar um Iterator pois de acordo com a documentação do Pyfirmata,
 # "Para usar portas analógicas, provavelmente é útil iniciar um thread do iterador. 
 # Caso contrário a placa continuará enviando dados para sua serial, até que ela transborde"
-# Ou seja, esses parâmetros servem como limite para não sobrecarregar a placa ARDUINO de informações
+# Ou seja, esses parâmetros servem como limite para não sobrecarregar a placa ARDUINO de informações.
 it = util.Iterator(board)
 it.start()
 time.sleep(0.05)
@@ -46,7 +42,7 @@ board.digital[servPin].mode = SERVO
 
 # Equivalente ao void loop();
 while True:
-    # Pega as informações de temperatura, e precipitação. Está dentro do loop para ser atualizado constantemente.
+    # Pega as informações de temperatura e precipitação. Está dentro do loop para ser atualizado constantemente.
     # As informações são encontradas através da pesquisa de class's e span's, pois pode haver mais de um de cada.
     # A função .text retira apenas o texto daquela função, e o .rstrip retira a porcentagem do valor de precipitação
     # a fim de ser mudada para um valor float.
@@ -56,13 +52,16 @@ while True:
 
     # É estúpido mas é necessário. A primeira vez que o sensor é lido, por algum motivo, retorna "None".
     # Por isso, é necessário executar esse loop try para pular essa condição.
+    # Quando o código tenta executar o cálculo de adição com o None, retornará um erro. Então, a expressão "except" reconhece
+    # esse erro e o "break" interrompe o loop, fazendo com que o código continue normalmente.
     try:
         analog_value = board.analog[0].read()
+        analog_value = analog_value + 1
         time.sleep(0.05)
     except:
         break
 
-    # Lê o sensor e executa os cáculos. O sensor retorna resultados em valor de resistência. Esses
+    # Lê o sensor e executa os cáculos. O sensor retorna resultados em valor de resistência (máx. 1023). Esses
     # cálculos transformam essas informações em porcentagens em consideração da comparação.
     analog_value = board.analog[0].read()
     moistData = analog_value * 1000
@@ -83,6 +82,8 @@ while True:
         msg = "Torneira DES."
 
     # É necessário de definir valores para "string" para que possam ser mostrados no display
+    # Já que o comando STRING_DATA// não permite mais de uma parâmetro, juntamos todos os caracteres em
+    # uma única variável.
     data1 = "Precipit.: " + str(prec) + "%"
     data2 = "Temp: " + str(temp) + " C"
     data3 = "Hum. Sensor: " + str(moistCalc) + "%"
